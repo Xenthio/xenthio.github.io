c21ead8ec28501dd7da07fa94f867c5f63aefe94
Xenthio <thegooster88@gmail.com>
2024-02-24 11:19:28 +1100

    upload

---
+++ stuff/launcher_d3d9_workshop_fullscreen.bat
@@ -0,0 +1,2 @@
+hl2 -allowdebug -dxlevel 90 -nod3d9ex +mat_dxlevel 90 -w 2560 -h 1440 -noborder -windowed -debug +sv_cheats 1 +r_3dsky 0 -insecure +r_WaterDrawReflection 0 +r_WaterDrawRefraction 0 +r_shadows 0 +r_frustumcullworld 0 +r_unloadlightmaps 1
+ 
