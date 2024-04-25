b0502c26e568076c5099f73a192e9a9a862e1aa2
Xenthio <thegooster88@gmail.com>
2024-04-25 13:30:41 +1000

    Update launcher_workshop_fullscreen_new.bat

--- stuff/launcher_workshop_fullscreen_new.bat
+++ stuff/launcher_workshop_fullscreen_new.bat
@@ -1,3 +1,3 @@
 del .\garrysmod\console.log
-hl2 -allowdebug -dxlevel 90 -nod3d9ex +mat_dxlevel 90 +mat_disable_d3d9ex 1 -w 2560 -h 1440 -noborder -windowed -debug +sv_cheats 1 +r_3dsky 0 +r_WaterDrawReflection 0 +r_WaterDrawRefraction 0 +mat_softwareskin 0 +r_shadows 0 +r_frustumcullworld 0 +r_unloadlightmaps 1 +mat_specular 0 +mat_bumpmap 0 +mat_normalmap 0 +mat_parallaxmap 0 +r_staticprop_lod 0 +r_lod 0 
+hl2 -allowdebug -dxlevel 90 -nod3d9ex +mat_dxlevel 90 +mat_disable_d3d9ex 1 -w 2560 -h 1440 -noborder -windowed -debug +sv_cheats 1 +r_3dsky 0 +r_WaterDrawReflection 0 +r_WaterDrawRefraction 0 +mat_softwareskin 0 +r_shadows 0 +r_frustumcullworld 0 +r_unloadlightmaps 1 +mat_specular 0 +mat_bumpmap 0 +mat_normalmap 0 +mat_parallaxmap 0 +r_staticprop_lod 0 +r_lod 0 +mat_forcedynamic 1
  
